32564ce99f5cf0c2b8e3dfe61efc1ac4ab1b8279
Juan Rodriguez <juansebastianrodriguezmateus7b@gmail.com>
2025-04-24 01:03:08 -0500

    Create structure project added createUser added validate Token Firebase

---
+++ Backend/PlayerManagment/src/main/java/co/edu/uptc/playermanagment/DTOs/LoginDTO.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.playermanagment.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginDTO {
+    private String nickname;
+    private String password;
+}
---
+++ Backend/PlayerManagment/src/main/java/co/edu/uptc/playermanagment/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package co.edu.uptc.playermanagment.repositories;
+import co.edu.uptc.playermanagment.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,String> {
+    boolean findUserByEmail(String email);
+}
