c522990274e12141eca0c20681e8c6f9b4e68880
SamuDevGit <samu.davargas@gmail.com>
2025-04-26 00:00:01 -0500

    menu_update

--- Frontend/login-app/src/app/app.routes.ts
+++ Frontend/login-app/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { MenuComponent } from './menu/menu.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'menu', component: MenuComponent }
 ];
