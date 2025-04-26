abb03fa46517f05325f96542f08ee1ffed3203cd
Sebastian <Sebastiandd87@gmail.com>
2025-04-26 17:47:04 -0500

    Conected wait room

---
+++ Backend/PlayerManagment/target/generated-sources/annotations/co/edu/uptc/playermanagment/mappers/UserMapperImpl.java
@@ -0,0 +1,43 @@
+package co.edu.uptc.playermanagment.mappers;
+
+import co.edu.uptc.playermanagment.DTOs.UserDTO;
+import co.edu.uptc.playermanagment.entities.User;
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-04-26T17:38:07-0500",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 23.0.2 (Amazon.com Inc.)"
+)
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public UserDTO userToDTO(User user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        UserDTO userDTO = new UserDTO();
+
+        userDTO.setNickname( user.getNickname() );
+        userDTO.setPassword( user.getPassword() );
+        userDTO.setEmail( user.getEmail() );
+
+        return userDTO;
+    }
+
+    @Override
+    public User DTOToUser(UserDTO user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        User user1 = new User();
+
+        user1.setNickname( user.getNickname() );
+        user1.setPassword( user.getPassword() );
+        user1.setEmail( user.getEmail() );
+
+        return user1;
+    }
+}
