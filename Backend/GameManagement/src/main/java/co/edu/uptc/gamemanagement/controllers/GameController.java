14f534e381c45a728f3efbb05eb1975f57b03370
Juan Rodriguez <juansebastianrodriguezmateus7b@gmail.com>
2025-04-27 12:56:54 -0500

    change post to get method existGame

--- Backend/GameManagement/src/main/java/co/edu/uptc/gamemanagement/controllers/GameController.java
+++ Backend/GameManagement/src/main/java/co/edu/uptc/gamemanagement/controllers/GameController.java
@@ -3,10 +3,7 @@ package co.edu.uptc.gamemanagement.controllers;
 import co.edu.uptc.gamemanagement.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/Game")
@@ -15,10 +12,9 @@ public class GameController {
     @Autowired
     private GameService gameService;
 
-    @PostMapping("/Check")
-    public ResponseEntity<Boolean> existGame(@RequestBody int code){
-        return ResponseEntity.ok(gameService.checkGame(code));
+    @GetMapping("/Check/{codeGame}")
+    public ResponseEntity<Boolean> existGame(@PathVariable int codeGame){
+        return ResponseEntity.ok(gameService.checkGame(codeGame));
     }
-
 
 }
