36f42460411cec491eb7c86cb4d4b6f0368e60d1
Juan Rodriguez <juansebastianrodriguezmateus7b@gmail.com>
2025-05-15 12:12:32 -0500

    Add configuration cors

--- Backend/ApiGateway/src/main/java/co/edu/uptc/apigateway/security/config/WebConfig.java
+++ Backend/ApiGateway/src/main/java/co/edu/uptc/apigateway/security/config/WebConfig.java
@@ -3,19 +3,20 @@ package co.edu.uptc.apigateway.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebFlux
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig implements WebFluxConfigurer {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
+    public WebFluxConfigurer corsConfigurer() {
+        return new WebFluxConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(org.springframework.web.reactive.config.CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:5173",
                                 "http://localhost:5177",
@@ -25,6 +26,7 @@ public class WebConfig implements WebMvcConfigurer {
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
+                WebFluxConfigurer.super.addCorsMappings(registry);
             }
         };
     }
