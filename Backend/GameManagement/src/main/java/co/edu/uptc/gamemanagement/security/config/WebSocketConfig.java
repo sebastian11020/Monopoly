04d92967c60d38b6ae2d866efe8612d43107044f
Juan Rodriguez <juansebastianrodriguezmateus7b@gmail.com>
2025-05-15 18:43:16 -0500

    Config cors api Gateway

---
+++ Backend/GameManagement/src/main/java/co/edu/uptc/gamemanagement/security/config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package co.edu.uptc.gamemanagement.security.config;
+
+import jdk.jfr.Enabled;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/Game");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/app") // Endpoint STOMP
+                .setAllowedOrigins("*"); // Para evitar problemas con CORS
+    }
+
+}
