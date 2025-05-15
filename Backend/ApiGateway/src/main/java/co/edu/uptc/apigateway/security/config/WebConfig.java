7d611fb85f74a0894608deecbb9b6f8a08920827
Juan Rodriguez <juansebastianrodriguezmateus7b@gmail.com>
2025-05-15 12:02:10 -0500

    Add configuration cors

--- Backend/ApiGateway/src/main/java/co/edu/uptc/apigateway/security/config/WebConfig.java
+++ Backend/ApiGateway/src/main/java/co/edu/uptc/apigateway/security/config/WebConfig.java
@@ -2,12 +2,13 @@ package co.edu.uptc.apigateway.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
+@EnableWebFlux
 public class WebConfig implements WebMvcConfigurer {
 
     @Bean
