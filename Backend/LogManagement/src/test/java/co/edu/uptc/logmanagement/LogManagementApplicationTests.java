6973276cacb93487f83b8d7bc0c6bf097e578637
Juan Rodriguez <juansebastianrodriguezmateus7b@gmail.com>
2025-05-24 10:40:23 -0500

    add LogManagement

---
+++ Backend/LogManagement/src/test/java/co/edu/uptc/logmanagement/LogManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.logmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
